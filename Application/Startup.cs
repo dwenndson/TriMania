47c4fe25c8467902f78924e9c5f023048b9fd840
dwenndson <diego.wenndson@gmail.com>
2021-07-23 13:40:37 -0300

    alteração startup

--- Application/Startup.cs
+++ Application/Startup.cs
@@ -1,4 +1,6 @@
 using Dominio.Entidades;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +31,7 @@ namespace Application
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<JwtSecret>(Configuration.GetSection("AppSettion"));
+                
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
